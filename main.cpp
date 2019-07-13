8636eb4dced4071afb170cbeece09c34ef8843ad
25077667 <zxc25077667@gmail.com>
2019-07-13 14:18:16 +0800

    listen key event for muiti thread

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include "header/userOperate.hpp"
+#include "header/writeJson.hpp"
+#include "header/screen.hpp"
+#include "header/role.hpp"
+#include "header/networking.hpp"
 using namespace std;
 
-int main(){
-    listenKeyBoard();
+int main() {
+    int* keyValue = new int;
+    usleep(100);
+    thread keyboardEvent(listenKeyBoard, keyValue);
+    while (true) {
+        if (*keyValue != -1) {
+            addch(char(*keyValue));
+            usleep(80); //the computer is too fast to lead inputs have one or more char 
+        }
+    }
+    keyboardEvent.join();
     return 0;
 }
