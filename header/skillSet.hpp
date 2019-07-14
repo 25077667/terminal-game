bf9dc89c9e3b89c2fdd7c4b2939d6aee4c17afca
25077667 <zxc25077667@gmail.com>
2019-07-14 22:44:07 +0800

    prevent double including

--- header/skillSet.hpp
+++ header/skillSet.hpp
@@ -1,16 +1,21 @@
+#ifndef SKILLSET_HPP
+#define SKILLSET_HPP
 #include "role.hpp"
 
 namespace TeamSkill {
 void noneSkill() {
 }
-void addHp((*Object::People) & current, std::vector<double>& argvs) {
+void addHp((*Object::People) & current, (std::vector<double>)&argvs) {
     current->setHp(current->getHp() + argvs.at(0));
 }
 }  // namespace TeamSkill
+
 namespace EnemySkill {
 void noneSkill() {
 }
-void addHp((*Object::People) & current, std::vector<double>& argvs) {
+void addHp((*Object::People) & current, (std::vector<double>)&argvs) {
     current->setHp(current->getHp() + argvs.at(0));
 }
 }  // namespace EnemySkill
+
+#endif
