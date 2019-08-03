91d3dea7ff331a3c6c1dcaea9b2fefa8202f7f87
25077667 <zxc25077667@gmail.com>
2019-08-03 23:18:18 +0800

    pass

--- header/skillSet.hpp
+++ header/skillSet.hpp
@@ -1,20 +1,21 @@
 #ifndef SKILLSET_HPP
 #define SKILLSET_HPP
+#include <vector>
 #include "role.hpp"
 
 namespace TeamSkill {
-void noneSkill() {
+void noneSkill(Object::People& current, std::vector<double>& argvs) {
 }
-void addHp((*Object::People) & current, (std::vector<double>)&argvs) {
-    current->setHp(current->getHp() + argvs.at(0));
+void addHp(Object::People& current, std::vector<double>& argvs) {
+    current.setHP(current.getHP() + argvs.at(0));
 }
 }  // namespace TeamSkill
 
 namespace EnemySkill {
-void noneSkill() {
+void noneSkill(Object::People& current, std::vector<double>& argvs) {
 }
-void addHp((*Object::People) & current, (std::vector<double>)&argvs) {
-    current->setHp(current->getHp() + argvs.at(0));
+void addHp(Object::People& current, std::vector<double>& argvs) {
+    current.setHP(current.getHP() + argvs.at(0));
 }
 }  // namespace EnemySkill
 
