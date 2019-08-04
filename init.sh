8c25ab4670ded60bcd01b207501e92da5f20fc3a
25077667 <zxc25077667@gmail.com>
2019-08-04 14:26:49 +0800

    install all tool for setting entire env.

--- init.sh
+++ init.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 sudo apt-get -y install build-essential libtcmalloc-minimal4 && sudo ln -s /usr/lib/libtcmalloc_minimal.so.4 /usr/lib/libtcmalloc_minimal.so
 sudo apt-get -y install libncurses-dev
+sudo apt-get -y install libboost-all-dev libssl-dev
 
-if [ "$(ls -l /dev/input/ | grep event4 | awk '{print $3}')"="root" ]; then
-    echo -n 'linten keyboard need root!'
-    echo
-fi
-
-
-read -n1 -p "Do you wnat to include devlopment mode? [Y/n]" doit
-
-case ${doit} in
-y | Y) sudo apt install libboost-all-dev libssl-dev ;;
-n | N)
-    echo
-    echo "Abort"
-    exit 0
-    ;;
-*) echo "Don\'t do that!" ;;
-esac
+#install all tool for setting entire env. 
