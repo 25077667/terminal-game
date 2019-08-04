c76d98240096e18e77a134e71835e9b2c264fcc9
25077667 <zxc25077667@gmail.com>
2019-08-04 15:03:20 +0800

    wget json and install to /usr/include/

--- init.sh
+++ init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+# install crow env. 
 sudo apt-get -y install build-essential libtcmalloc-minimal4 && sudo ln -s /usr/lib/libtcmalloc_minimal.so.4 /usr/lib/libtcmalloc_minimal.so
-sudo apt-get -y install libncurses-dev
 sudo apt-get -y install libboost-all-dev libssl-dev
+sudo apt-get -y install libboost-dev
+
+# install curses for listen keyboard
+sudo apt-get -y install libncurses-dev
+
+#install zip packages
+sudo apt-get -y install zip unzip 
+wget https://github.com/nlohmann/json/releases/download/v3.7.0/include.zip
+unzip include.zip -d /usr/
+rm include.zip
 
 #install all tool for setting entire env. 
