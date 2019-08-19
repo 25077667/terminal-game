30fa851b6072a1b975e2b738ddd4d0d911cfc644
25077667 <zxc25077667@gmail.com>
2019-08-19 17:05:03 +0800

    define game clearly

--- header/skillSet.hpp
+++ header/skillSet.hpp
@@ -6,22 +6,22 @@
 namespace TeamSkill {
 void noneSkill(Object::People& _this, std::vector<unsigned int>& argvs) {
 }
-void addHp(Object::People& _this, std::vector<unsigned int>& argvs) {
-    _this.setHP(_this.getHP() + argvs.at(1));
+void addScore(Object::People& _this, std::vector<unsigned int>& argvs) {
+    _this.setScore(_this.getScore() + argvs.at(1));
 }
 void minusHP(Object::People& _this, std::vector<unsigned int>& argvs){
-    _this.setHP(_this.getHP()-argvs.at(1));
+    _this.setScore(_this.getScore()-argvs.at(1));
 }
 }  // namespace TeamSkill
 
 namespace EnemySkill {
 void noneSkill(Object::People& _this, std::vector<unsigned int>& argvs) {
 }
-void addHp(Object::People& _this, std::vector<unsigned int>& argvs) {
-    _this.setHP(_this.getHP() + argvs.at(1));
+void addScore(Object::People& _this, std::vector<unsigned int>& argvs) {
+    _this.setScore(_this.getScore() + argvs.at(1));
 }
 void minusHP(Object::People& _this, std::vector<unsigned int>& argvs) {
-    _this.setHP(_this.getHP() - argvs.at(1));
+    _this.setScore(_this.getScore() - argvs.at(1));
 }
 }  // namespace EnemySkill
 
